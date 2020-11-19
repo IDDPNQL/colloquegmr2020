8d242e4eee92325d4740fde751f880752ce9c5e5
IDDPNQL <61841279+IDDPNQL@users.noreply.github.com>
2020-11-19 13:22:28 -0500

    Update custom-scripts.js

--- app/custom-scripts.js
+++ app/custom-scripts.js
@@ -13,7 +13,7 @@ require(["dojo/topic","dijit/Dialog"], function(topic, Dialog) {
 	// Custom script to add a splash page
     	var splashPage = new Dialog ({
 		title: "<p><strong>Instructions</strong></p>",
-		content: "<p><strong>Portraits spécifiques</strong></p><p style='text-align: justify'><img src='https://iddpnql.maps.arcgis.com/sharing/rest/content/items/c4eab06fe02c4b9d896ff3f24577436a/data' width='50' height='50'>Naviguez sur la carte à droite de l'écran et cliquez sur une communauté pour obtenir un portrait des infrastructures de gestion des M.R. accessibles ou défilez les onglets à gauche de l'écran.</p>",
+		content: "<p><strong>Portraits spécifiques</strong></p><p style='text-align: justify'><img src='https://iddpnql.maps.arcgis.com/sharing/rest/content/items/c4eab06fe02c4b9d896ff3f24577436a/data' width='150' height='150'>Naviguez sur la carte à droite de l'écran et cliquez sur une communauté pour obtenir un portrait des infrastructures de gestion des M.R. accessibles ou défilez les onglets à gauche de l'écran.</p>",
 		style: 'width: 430px; height: 436px;'
 	});
 	
